ec11efec5daaf34ec6183c8b1b91a82b81b8fe9b
Abdullah Almkseky <147331885+o0wzza0o@users.noreply.github.com>
2026-05-26 23:10:48 +0300

    Update Auth.jsx

--- web/src/pages/Auth.jsx
+++ web/src/pages/Auth.jsx
@@ -55,11 +55,8 @@ export default function Auth() {
             </button>
           </p>
 
-     /*   <button onClick={google} type="button" className="btn-secondary w-full mt-5">
-            <svg className="w-4 h-4" viewBox="0 0 48 48"><path fill="#FFC107" d="M43.6 20.5H42V20H24v8h11.3C33.7 32.4 29.3 36 24 36c-6.6 0-12-5.4-12-12s5.4-12 12-12c3 0 5.7 1.1 7.8 2.9l5.7-5.7C34.5 6.1 29.5 4 24 4 12.9 4 4 12.9 4 24s8.9 20 20 20c11 0 19.7-8.4 19.7-20 0-1.2-.1-2.4-.3-3.5z"/><path fill="#FF3D00" d="M6.3 14.7l6.6 4.8C14.7 16 19 12 24 12c3 0 5.7 1.1 7.8 2.9l5.7-5.7C34.5 6.1 29.5 4 24 4 16.4 4 9.8 8.4 6.3 14.7z"/><path fill="#4CAF50" d="M24 44c5.3 0 10.1-2 13.7-5.3l-6.3-5.3C29.6 35 27 36 24 36c-5.3 0-9.7-3.6-11.3-8.4l-6.5 5C9.6 39.5 16.2 44 24 44z"/><path fill="#1976D2" d="M43.6 20.5H42V20H24v8h11.3c-.8 2.3-2.3 4.3-4.2 5.6l6.3 5.3c-.5.4 6.6-4.8 6.6-14.9 0-1.2-.1-2.4-.4-3.5z"/></svg>
-            Continue with Google
-          </button>
-*/
+      
+          
           <div className="my-5 flex items-center gap-3 text-xs text-slate-400">
             <div className="flex-1 h-px bg-slate-200 dark:bg-slate-800" />or<div className="flex-1 h-px bg-slate-200 dark:bg-slate-800" />
           </div>
